bd543b7d1d68faa2b476dd7fb0812230d14e1b4a
Raymond Chow <raychow1944@yahoo.com>
2018-11-17 22:54:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('.target');
+}
+
+function deepestChild(){
+  let children = document.getElementById('grand-node').querySelectorAll('div');
+  return children[children.length-1];
+}
+
+function increaseRankBy(n){
+  let ranks = document.querySelectorAll('.ranked-list li');
+  for(let i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n;
+  }
+}
