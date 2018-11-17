9159987ef9904334e660386986aa69c95821aad1
Raymond Chow <raychow1944@yahoo.com>
2018-11-17 22:44:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function getFirstSelector(selector) {
 function nestedTarget(){
   return document.getElementById('nested').querySelector('.target');
 }
+
+function deepestChild(){
+  return document.getElementById('grand-node').querySelectorAll('div');
+}
